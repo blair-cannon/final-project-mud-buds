9555e100be180fbd0db10993f624765eb327c9b5
Blair Preston <blair.preston@student.awesomeinc.org>
2022-04-26 19:01:03 +0000

    bootstrap col and rows for sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,42 +1,63 @@
 import 'react-bootstrap-drawer/lib/style.css';
-    import React, { useState } from 'react';
-    import {
+import React, { useState } from 'react';
+import {
         Col,
         Collapse,
         Container,
         Row,
     } from 'react-bootstrap';
-    import { Drawer, } from 'react-bootstrap-drawer';
+import { Drawer, } from 'react-bootstrap-drawer';
+import DogDisplay from '../components/dogdisplay';
 
-function SidebarSetUp() {
-    const [open, setOpen] = useState(false);
-    const handleToggle = () => setOpen(!open);
-  return (
-    <div>
-            <Drawer { ...props }>
-                <Drawer.Toggle onClick={ handleToggle } />
-                <Collapse in={ open }>
-                    <Drawer.Overflow>
-                        <Drawer.ToC>
-                            <Drawer.Header href="/">Application</Drawer.Header>
-                            <Drawer.Nav>
-                                <Drawer.Item href="/settings">Settings</Drawer.Item>
-                            </Drawer.Nav>
-                        </Drawer.ToC>
-                    </Drawer.Overflow>
-                </Collapse>
-            </Drawer>
-    </div>
-        )
-};
+// function SidebarSetUp() {
+//     const [open, setOpen] = useState(false);
+//     const handleToggle = () => setOpen(!open);
+//   return (
+//     <div>
+//             <Drawer>
+//                 <Drawer.Toggle onClick={ handleToggle } />
+//                 <Collapse in={ open }>
+//                     <Drawer.Overflow>
+//                         <Drawer.ToC>
+//                             <Drawer.Header to="/">Application</Drawer.Header>
+//                             <Drawer.Nav>
+//                                 <Drawer.Item to="/settings">Settings</Drawer.Item>
+//                             </Drawer.Nav>
+//                         </Drawer.ToC>
+//                     </Drawer.Overflow>
+//                 </Collapse>
+//             </Drawer>
+//     </div>
+//         )
+// };
 
     
 export default function SideBar(props) {
         return (
             <Container fluid>
                 <Row className="flex-xl-nowrap">
-                    <Col as={ SidebarSetUp } xs={ 12 } md={ 3 } lg={ 2 } />
-                    <Col xs={ 12 } md={ 9 } lg={ 10 }>{ props.children }</Col>
+                    <Col className="messageCol" lg={ 4 }>
+                        <p>Ang Lorem Ipsum ay ginagamit na modelo ng industriya ng pagpriprint at pagtytypeset. 
+                            Ang Lorem Ipsum ang naging regular na modelo simula pa noong 1500s, noong may isang 
+                            di kilalang manlilimbag and kumuha ng galley ng type at ginulo ang pagkaka-ayos nito
+                            upang makagawa ng libro ng mga type specimen. Nalagpasan nito hindi lang limang siglo, 
+                            kundi nalagpasan din nito ang paglaganap ng electronic typesetting at nanatiling parehas. 
+                            Sumikat ito noong 1960s kasabay ng pag labas ng Letraset sheets na mayroong mga talata ng Lorem Ipsum, 
+                            at kamakailan lang sa mga desktop publishing software tulad ng Aldus Pagemaker ginamit ang mga bersyon 
+                            ng Lorem Ipsum. </p>
+
+                             </Col>
+                    <Col className="contentCol" lg={ 8 } ><DogDisplay />
+                    <p>Ang Lorem Ipsum ay ginagamit na modelo ng industriya ng pagpriprint at pagtytypeset. 
+                            Ang Lorem Ipsum ang naging regular na modelo simula pa noong 1500s, noong may isang 
+                            di kilalang manlilimbag and kumuha ng galley ng type at ginulo ang pagkaka-ayos nito
+                            upang makagawa ng libro ng mga type specimen. Nalagpasan nito hindi lang limang siglo, 
+                            kundi nalagpasan din nito ang paglaganap ng electronic typesetting at nanatiling parehas. 
+                            Sumikat ito noong 1960s kasabay ng pag labas ng Letraset sheets na mayroong mga talata ng Lorem Ipsum, 
+                            at kamakailan lang sa mga desktop publishing software tulad ng Aldus Pagemaker ginamit ang mga bersyon 
+                            ng Lorem Ipsum. </p>
+
+                    </Col>
                 </Row>
             </Container>
         );
--- src/routes/feed.js
+++ src/routes/feed.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import DogDisplay from '../components/dogdisplay';
+
 import SideBar from '../components/sidebar'
 
 export default function Feed() {
   return (
     <div>
-      <DogDisplay />
       <SideBar />
     </div>
 
