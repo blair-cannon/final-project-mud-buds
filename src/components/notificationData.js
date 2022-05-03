53b211d2fbdf64db14af27ef597f84ddd9f81f1f
Blair Preston <blair.preston@student.awesomeinc.org>
2022-05-03 20:33:13 +0000

    patch request function

--- src/components/notificationData.js
+++ src/components/notificationData.js
@@ -3,6 +3,19 @@ import { Dropdown } from 'react-bootstrap';
 import request from '../services/api.requests.js';
 import { useGlobalState } from "../context/GlobalState";
 
+async function acceptConnection({ notification }) {
+    let options = {
+        url: `/connections/${notification.id}/`,
+        method: 'PATCH',
+        body: JSON.stringify({"is_accepted": true})
+      } 
+      let variable = await request(options)
+      // .then(response => { console.log(response.status);
+      //   return response.json();  })  
+      // .then(data => console.log(data));
+
+    // update boolean is_accepted to true ?
+  }
 
 export default function Convos() {
   const [ state, dispatch ] = useGlobalState();
@@ -33,19 +46,10 @@ const Notification = ({ notification }) => {
         <div>
             <li>
                 {notification.dog_initializer.name}
-                <button onClick={() => {AcceptConnection({ notification })}}>check</button>
+                <button onClick={() => {acceptConnection({ notification })}}>check</button>
                 <button>X</button>
             </li>
         </div>
       )
     }
    
-async function AcceptConnection({ notification }) {
-    // let options = {
-    //     url: `/connections/${notification.id}`,
-    //     method: 'PATCH',
-    // } 
-    // let resp = await request(options)
-    // setConversations(resp.data)
-    // update boolean is_accepted to true ?
-}
