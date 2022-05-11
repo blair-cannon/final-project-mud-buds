1d5d2a4c51cd58059f24b0cba674b6731097b17c
Blair Preston <blair.preston@student.awesomeinc.org>
2022-05-11 03:57:43 +0000

    cool loading screen like tinder

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react'
+import LoadingScreen from './components/loadingScreen';
 import Feed from './routes/feed';
 import Notifications from './routes/notifications';
 import Home from './routes/home';
@@ -9,22 +11,40 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GlobalProvider } from './context/GlobalState';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000)
+  }, [])
+
 return (
-<GlobalProvider >
-  <div className="App">
-  <Router>
-    <Navigation></Navigation>
-    <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/feed" element={<Feed />}></Route>
-        <Route path="/notifications" element={<Notifications />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-    </Routes>
-  </Router>
-  </div>
-</GlobalProvider>
+  <>
+    {loading === false ? (
+    <div className="App">
+    <GlobalProvider >
+    <div className="App">
+    <Router>
+      <Navigation></Navigation>
+      <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/feed" element={<Feed />}></Route>
+          <Route path="/notifications" element={<Notifications />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </Router>
+    </div>
+    </GlobalProvider>
+      </div>
+        ) : (
+          <LoadingScreen />
+        )}
+  </>
 );
 }
 export default App;
+
+
+
+
---
+++ src/components/loadingScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PawPrint from '../images/whitepawprint.svg';
+
+export default function LoadingScreen() {
+  return (
+    <div className="loading-screen-wrapper">
+        {/* <img className="paw-print-svg" src={PawPrint} alt="paw print"/>
+        <img className="paw-print-svg" src={PawPrint} alt="paw print"/> */}
+        <img className="paw-print-svg" src={PawPrint} alt="paw print"/>
+    </div>
+  )
+}
